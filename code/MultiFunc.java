a554b99c36226655b83b89a6f6c25d49d1d024c2
Chloe Greene <thecatliferules@gmail.com>
2024-02-26 21:25:46 -0500

    a

--- code/MultiFunc.java
+++
@@ -1,9 +0,0 @@
-public class MultiFunc {
-    public static void main(String[] args) {
-        System.out.println("Hello, From Main Func!"); 
-    }
-
-    public static void hidden_func(){
-        System.out.println("This Function should never be ran, if you see this, oopsies"); 
-    }
-}
--- code/Vars.java
+++
@@ -1,9 +0,0 @@
-class Vars {
-    public static void main(String[] args) {
-
-        int a = 2;
-        int b = 3;
-        int c = a+b;
-        System.out.println(c); 
-    }
-}
